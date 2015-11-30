140bf5b1b2f5321c2078f77577b0d04ebb4a59b5
florianpdf <florian.pdf@gmail.com>
2015-11-30 10:37:14 +0100

    Revert "Dg/test edit doc"

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,11 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new UserBundle\UserBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new GedBundle\GedBundle(),
-            new AgendaBundle\AgendaBundle(),
-
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
