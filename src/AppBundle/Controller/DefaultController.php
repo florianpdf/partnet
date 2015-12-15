646286beb90f03bfc90419bd7d34f922c1e72ba1
Florian Grandjean <florian.pdf@gmail.com>
2015-12-15 13:14:26 +0100

    1 - Ajout de la fonction dans DocumentRepository permettant de compter le nb de doc uploader pendant le mois 2 - Ajout dans la vue app/Ressource/views/default/index.html.twig 3 - Suppression de src/GedBundle/Controller/DefaultController.php 4 - Ajout du retour de la viariable nb_month dans DocumentsController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,12 +15,14 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $nb = $em->getRepository('GedBundle:Documents')->getNbDocuments();
+        $nb_month = $em->getRepository('GedBundle:Documents')->getNbDocumentsMonth();
 
         if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
             return $this->redirectToRoute('fos_user_security_login');
 
         return $this->render('default/index.html.twig', array(
-            'nb' => $nb)
-        );
+            'nb' => $nb,
+            'nb_month' => $nb_month
+        ));
     }
 }
