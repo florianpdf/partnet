b8e9eb811ea859aee4ac4c5d31171e57a2661ad6
Florian Grandjean <florian.pdf@gmail.com>
2015-12-19 00:18:23 +0100

    Ajout de test sur user bundle
    1 - Ajout des test sur le profile utilisateur ==> show + edit
    2 - Suppression de l'affichage du username dans le
    show_content.html.twig

--- src/UserBundle/Tests/Controller/UserControllerTest.php
+++ src/UserBundle/Tests/Controller/UserControllerTest.php
@@ -1,129 +1,126 @@
 <?php
 
-namespace UserBundle\Tests\Controller;
+namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class UserControllerTest extends WebTestCase
+class DefaultControllerTest extends WebTestCase
 {
-    // Vérification des champs dans le formulaire de login
-    public function testChampsLogin ()
+    // Création d'un client User
+    public function UserConnection()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/login');
-
-        $this->assertTrue($crawler->filter('form input[name="_username"]')->count() == 1);
-        $this->assertTrue($crawler->filter('form input[name="_password"]')->count() == 1);
-    }
-
-    // Test de connexion en temps qu'administrateur, nécessite de loader les fixtures
-    public function testLoginAdmin()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/login');
-
-        // Sélection basée sur la valeur, l'id ou le nom des boutons
-        $form = $crawler->selectButton('Connexion')->form();
-
-        $form['_username']= 'admin@admin.com';
-        $form['_password']= 'admin';
-
-        $crawler = $client->submit($form);
-
-        // Il faut suivre la redirection
-        $crawler = $client->followRedirect();
-        $this->assertEquals('AppBundle\Controller\DefaultController::indexAction',
-            $client->getRequest()->attributes->get('_controller'));
-    }
-
-    // Test de connexion en temps qu'utilisateur, nécessite de loader les fixtures
-    public function testLoginUser ()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/login');
-
-        // Sélection basée sur la valeur, l'id ou le nom des boutons
-        $form = $crawler->selectButton('Connexion')->form();
-
-        $form['_username'] = 'user@user.com';
-        $form['_password'] = 'user';
-
-        $crawler = $client->submit($form);
-
-        // Il faut suivre la redirection
-        $crawler = $client->followRedirect();
-        $this->assertEquals('AppBundle\Controller\DefaultController::indexAction',
-            $client->getRequest()->attributes->get('_controller'));
-    }
-
-    // Test de connexion en temps qu'utilisateur non inscrit, nécessite de loader les fixtures
-    public function testLoginBidonUser ()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/login');
-
-        // Sélection basée sur la valeur, l'id ou le nom des boutons
-        $form = $crawler->selectButton('Connexion')->form();
-
-        $form['_username']= 'Nimportequoi';
-        $form['_password']= 'rien';
-
-        $crawler = $client->submit($form);
-
-        // Il faut suivre la redirection
-        $crawler = $client->followRedirect();
-        $this->assertEquals('UserBundle\Controller\SecurityController::loginAction',
-            $client->getRequest()->attributes->get('_controller'));
-    }
-
-    /*
-    public function testCompleteScenario()
-    {
-        // Create a new client to browse the application
-        $client = static::createClient();
-
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/user/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /user/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'userbundle_user[field_name]'  => 'Test',
-            // ... other fields to fill
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'user@user.com',
+            'PHP_AUTH_PW' => 'user',
         ));
+        return $client;
+    }
+
+    // Test du lien pour acceder au profile
+    public function testLinkProfile()
+    {
+        $client = $this->UserConnection();
+
+        // Test du lien "accès profil" d'Émilie Perrin
+        $crawler = $client->request('GET', '/');
+        $link = $crawler
+            ->filter('a:contains("Émilie")')
+            ->eq(0)
+            ->link();
+        $crawler = $client->click($link);
+        $this->assertEquals('UserBundle\Controller\ProfileController::showAction',
+            $client->getRequest()->attributes->get('_controller'));
+    }
+
+    // Vérification des infos qui s'affichent
+    public function testShowProfile()
+    {
+        $client = $this->UserConnection();
+
+        $crawler = $client->request('GET', '/profile/');
+
+        // Verification de l'affichage des infos du profile
+        $this->assertEquals(1, $crawler->filter('html:contains("Prenom: Émilie")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Nom: Perrin")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Email: user@user.com")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Organisme")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Fonction: Chargée d\'accueil")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Téléphone: 0368474510")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Modifier mon profil")')->count());
+
+        // Vérification de l'action cibler lors du click sur "Modifier mon profil"
+        $link = $crawler->selectLink('Modifier mon profil')->link();
+        $crawler = $client->click($link);
+        $this->assertEquals('UserBundle\Controller\ProfileController::editAction',
+            $client->getRequest()->attributes->get('_controller'));
+    }
+
+    // Vérification des champs du formulaire
+    public function testChampFormEditProfile()
+    {
+        $client = $this->UserConnection();
+
+        $crawler = $client->request('GET', '/profile/edit');
+        $this->assertEquals('UserBundle\Controller\ProfileController::editAction',
+            $client->getRequest()->attributes->get('_controller'));
+
+        // Tests de la présence des champs
+        $this->assertTrue($crawler->filter('form input[name="fos_user_profile_form[email]"]')->count() == 1);
+        $this->assertTrue($crawler->filter('form input[name="fos_user_profile_form[current_password]"]')->count() == 1);
+        $this->assertTrue($crawler->filter('form select[name="fos_user_profile_form[organisme]"]')->count() == 1);
+        $this->assertTrue($crawler->filter('form input[name="fos_user_profile_form[telephone]"]')->count() == 1);
+        $this->assertTrue($crawler->filter('form input[name="fos_user_profile_form[poste]"]')->count() == 1);
+        $this->assertTrue($crawler->filter('form input[name="fos_user_profile_form[nom]"]')->count() == 1);
+        $this->assertTrue($crawler->filter('form input[name="fos_user_profile_form[prenom]"]')->count() == 1);
+        $this->assertTrue($crawler->filter('form input[name="fos_user_profile_form[file]"]')->count() == 1);
+    }
+
+    // Test edition du profile
+    public function editUser()
+    {
+        $client = $this->UserConnection();
+
+        $crawler = $client->request('GET', 'admin/documents/nouveau');
+
+        // Soumission du formulaire avec les modifs
+        $form = $crawler->selectButton('Envoyer')->form(array_merge(array(
+            'fos_user_profile_form[email]' => 'user_test_edit@user.com',
+            'fos_user_profile_form[current_password]' => 'user',
+            'fos_user_profile_form[organisme]' => 'CAP Emploi',
+            'fos_user_profile_form[telephone]' => '0123456789',
+            'fos_user_profile_form[poste]' => 'Secretaire',
+            'fos_user_profile_form[nom]' => 'name_user_test_edit',
+            'fos_user_profile_form[prenom]' => 'username_test_edit',
+            'gedbundle_documents[file]' => __DIR__ . '/../../../../web/test_document/test.png'
+        )));
 
         $client->submit($form);
-        $crawler = $client->followRedirect();
 
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+        // Vérification de l'action renvoyé
+        $this->assertEquals('UserBundle\Controller\ProfileController::showAction',
+            $client->getRequest()->attributes->get('_controller'));
 
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
+        // Vérification que les infos ont bien été modifié dans l'affichage du profile
+        $this->assertEquals(1, $crawler->filter('html:contains("Prenom: username_test_edit")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Nom: name_user_test_edit")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Email: user_test_edit@user.com")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Organisme")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Fonction: Secretaire")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Téléphone: 0123456789")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Modifier mon profil")')->count());
 
-        $form = $crawler->selectButton('Update')->form(array(
-            'userbundle_user[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
+        // Verification de l'enregistrement des modifs dans la BDD
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
 
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $query = $em->createQuery('SELECT count(d.id) from UserBundle:User u WHERE u.Prenom = :prenom AND u.Nom = :nom AND u.Email = :email AND u.Fonction = :fonction AND u.Téléphone = :telephone');
+        $query->setParameter('prenom', 'username_test_edit');
+        $query->setParameter('nom', 'name_user_test_edit');
+        $query->setParameter('email', 'user_test_edit@user.com');
+        $query->setParameter('fonction', 'Secretaire');
+        $query->setParameter('telephone', '0123456789');
+        $this->assertTrue(0 < $query->getSingleScalarResult());
     }
 
-    */
 }
