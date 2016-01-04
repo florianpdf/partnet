3ad0761be8341c0ace6be1fb2a74356e31112169
Florian Grandjean <florian.pdf@gmail.com>
2016-01-04 11:18:59 +0100

    installation du calendrier, ajout du js et du css, création d'un fichier calendarconfig contenant la config js du calendrier

---
+++ src/AgendaBundle/Resources/public/js/3calendarconfig.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title'
+        }
+    })
+
+});
