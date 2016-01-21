3b7be756fbbfc0f68df9eeb4e4bf175e08927f0a
Florian Grandjean <florian.pdf@gmail.com>
2016-01-21 16:24:53 +0100

    debug register user

--- src/UserBundle/Form/RegistrationType.php
+++ src/UserBundle/Form/RegistrationType.php
@@ -13,7 +13,7 @@ class RegistrationType extends AbstractType
     {
         $builder
             ->remove('username')
-            ->add('roles', 'choice', array('label' => 'Statut'))
+            //->add('roles', 'choice', array('label' => 'Statut'))
             ->add('id_organisme', 'entity', array(
                 'class'    => 'AppBundle:Organisme',
                 'property' => 'id',
