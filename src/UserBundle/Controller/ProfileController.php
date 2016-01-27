dce23d7ca7babcced8e01fffde6d939f76ffa659
Florian Grandjean <florian.pdf@gmail.com>
2016-01-27 23:49:29 +0100

    CORECTION DE BUG
    1 - L'image du profil se supprime si on la modifie
    2 - Correction de fautes d'orthographe sur page index

--- src/UserBundle/Controller/ProfileController.php
+++ src/UserBundle/Controller/ProfileController.php
@@ -76,6 +76,13 @@ class ProfileController extends Controller
 
         if ($form->isValid()) {
 
+            if($form->get('file')->getData() != null) {
+                if($user->getPictureName() != null) {
+                    unlink(__DIR__.'/../../../app/uploads/profile_pictures/'.$user->getPicture());
+                    $user->setPicture(null);
+                }
+            }
+
             $user->preUpload();
             $user->setUsername($this->getUser()->getEmail());
 
