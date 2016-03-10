8463510db9ff75e90c37a2a047dcaf736c989d48
Florian Grandjean <florian.pdf@gmail.com>
2016-03-10 12:07:21 +0100

    FIX BUG DELETE USER
    1 Correction du bug lors de la suppression des user
    2 Ajout de delete on cascade dans l'entité statistique"

--- src/AnnuaireBundle/Controller/DefaultController.php
+++ src/AnnuaireBundle/Controller/DefaultController.php
@@ -11,9 +11,14 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('UserBundle:User')->findAll();
+        $organisme_user_connect = $this->getUser()->getIdOrganisme();
+        $id_user_connect = $this->getUser()->getId();
 
         return $this->render('AnnuaireBundle:Default:index.html.twig', array(
-            'entities' => $entities
+            'entities' => $entities,
+            'organisme_user_connect' => $organisme_user_connect,
+            'id_user_connect' => $id_user_connect
+
         ));
     }
 }
